1f7ef0adf28b3a26cca60dc0441c1fd13e8adaee
sasha1Gury <sashagury456123@mail.ru>
2024-05-27 00:13:30 +0300

    feat: add MetaStatistic

---
+++ stat-service/stat-server/src/main/java/ru/practicum/stat/server/model/MetaStatistic.java
@@ -0,0 +1,31 @@
+package ru.practicum.stat.server.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@Table(name = "meta_statistic")
+@NoArgsConstructor
+@AllArgsConstructor
+public class MetaStatistic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private long id;
+
+    private String appName;
+
+    private String uri;
+
+    private String ip;
+
+    private LocalDateTime timestamp;
+}
