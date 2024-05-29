039c91be7c2e8a5c6f69f1a9779de91fa215add8
sasha1Gury <sashagury456123@mail.ru>
2024-05-29 23:26:46 +0300

    feat: add getStatistic

--- stat-service/stat-server/src/main/java/ru/practicum/stat/server/service/StatServerService.java
+++ stat-service/stat-server/src/main/java/ru/practicum/stat/server/service/StatServerService.java
@@ -2,19 +2,38 @@ package ru.practicum.stat.server.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.practicum.stat.server.dto.MetaStatisticDto;
-import ru.practicum.stat.server.model.MetaStatistic;
+import ru.practicum.stat.common.dto.RecordStatisticDto;
+import ru.practicum.stat.common.dto.ViewStatisticDto;
+import ru.practicum.stat.server.model.RecordStatistic;
 import ru.practicum.stat.server.model.MetaStatisticMapper;
 import ru.practicum.stat.server.repository.StatServerRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class StatServerService {
     private final StatServerRepository statServerRepository;
 
-    public MetaStatistic save(MetaStatisticDto metaStatisticDto) {
-        MetaStatistic metaStatistic = MetaStatisticMapper.toEntity(metaStatisticDto);
-        return statServerRepository.save(metaStatistic);
+    public void save(RecordStatisticDto recordStatisticDto) {
+        RecordStatistic recordStatistic = MetaStatisticMapper.toEntity(recordStatisticDto);
+        statServerRepository.save(recordStatistic);
     }
 
+    public List<ViewStatisticDto> getStatistic(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique) {
+        if (uris.isEmpty()) {
+            if (!unique) {
+                return statServerRepository.findStatistic(start, end);
+            } else {
+                return statServerRepository.findStatisticUniqueIp(start, end);
+            }
+        } else {
+            if (!unique) {
+                return statServerRepository.findStatisticForUriList(start, end, uris);
+            } else {
+                return statServerRepository.findStatisticForUriListAndUniqueIp(start, end, uris);
+            }
+        }
+    }
 }
