be949985be9ca07a1b8fd4832bb8205e64a66b75
sasha1Gury <sashagury456123@mail.ru>
2024-06-11 22:51:23 +0300

    feat: add User

---
+++ ewm-service/src/main/java/ru/practicum/ewm/service/user/model/User.java
@@ -0,0 +1,23 @@
+package ru.practicum.ewm.service.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private long id;
+
+    private String name;
+
+    private String email;
+}
