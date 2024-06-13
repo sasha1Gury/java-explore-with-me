739e83022738148be478a25c0afbf659e9724179
sasha1Gury <sashagury456123@mail.ru>
2024-06-13 10:12:40 +0300

    feat: add methods CategoryAdminController

--- ewm-service/src/main/java/ru/practicum/ewm/service/category/service/CategoryService.java
+++ ewm-service/src/main/java/ru/practicum/ewm/service/category/service/CategoryService.java
@@ -1,10 +1,12 @@
 package ru.practicum.ewm.service.category.service;
 
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import ru.practicum.ewm.service.category.dto.CategoryDto;
 import ru.practicum.ewm.service.category.model.Category;
 import ru.practicum.ewm.service.category.repository.CategoryRepository;
 import ru.practicum.ewm.service.event.service.EventService;
@@ -14,59 +16,62 @@ import ru.practicum.ewm.service.exceptions.CategoryNotFoundException;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
     private final CategoryRepository categoryRepository;
     private final EventService eventService;
+    private final ModelMapper mapper = new ModelMapper();
 
-    public Category getCategoryById(long categoryId) {
-        Optional<Category> optionalCategory = categoryRepository.findById(categoryId);
-        if (optionalCategory.isEmpty()) {
-            throw new CategoryNotFoundException("Категория " + categoryId + " не найдена");
-        } else {
-            return optionalCategory.get();
-        }
+    public CategoryDto getCategoryById(long categoryId) {
+        Category category = categoryRepository.findById(categoryId)
+        .orElseThrow(() -> new CategoryNotFoundException("Категория " + categoryId + " не найдена"));
+
+        return mapper.map(category, CategoryDto.class);
     }
 
-    public List<Category> getAllCategories(int from, int size) {
+    public List<CategoryDto> getAllCategories(int from, int size) {
         Pageable page = PageRequest.of(from/size, size);
-        return categoryRepository.findAll(page).getContent();
+        return categoryRepository.findAll(page).getContent()
+                .stream()
+                .map(category ->  mapper.map(category, CategoryDto.class))
+                .collect(Collectors.toList());
     }
 
-    public Category create(Category category) {
+    public CategoryDto create(CategoryDto category) {
         Category storageCategory;
         try {
-            storageCategory = categoryRepository.save(category);
+            storageCategory = categoryRepository.save(mapper.map(category, Category.class));
         } catch (DataIntegrityViolationException e) {
             throw new CategoryNameNotUniqueException("Название категории не уникально");
         }
 
-        return storageCategory;
+        return mapper.map(storageCategory, CategoryDto.class);
     }
 
-    public Category update(Category category, long categoryId) {
+    public CategoryDto update(CategoryDto category, long categoryId) {
         Category storageCategory;
 
         getCategoryById(categoryId);
         category.setId(categoryId);
 
         try {
-            storageCategory = categoryRepository.save(category);
+            storageCategory = categoryRepository.save(mapper.map(category, Category.class));
         } catch (DataIntegrityViolationException e) {
             throw new CategoryNameNotUniqueException("Название категории не уникально");
         }
 
-        return storageCategory;
+        return mapper.map(storageCategory, CategoryDto.class);
     }
 
     public void delete(long categoryId) {
-        Category category = getCategoryById(categoryId);
+        CategoryDto category = getCategoryById(categoryId);
         if (eventService.getEventsCountByCategoryId(categoryId) > 0) {
             throw new CategoryHaveLinkedEventsException("С категорией " + categoryId + " связаны события");
         } else {
-            categoryRepository.delete(category);
+            categoryRepository.delete(mapper.map(category, Category.class));
         }
     }
 }
