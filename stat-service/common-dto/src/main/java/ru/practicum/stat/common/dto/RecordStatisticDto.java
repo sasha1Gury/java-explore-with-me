cdc0ac4424db49366b61fe9775d29374fff72b21
sasha1Gury <sashagury456123@mail.ru>
2024-06-13 17:10:13 +0300

    feat: add builder

--- stat-service/common-dto/src/main/java/ru/practicum/stat/common/dto/RecordStatisticDto.java
+++ stat-service/common-dto/src/main/java/ru/practicum/stat/common/dto/RecordStatisticDto.java
@@ -1,12 +1,14 @@
 package ru.practicum.stat.common.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class RecordStatisticDto {
