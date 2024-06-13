ea02b26f2374b2d0a725a09a439acceba8b4e7df
sasha1Gury <sashagury456123@mail.ru>
2024-06-13 16:59:26 +0300

    feat: add Participation

---
+++ ewm-service/src/main/java/ru/practicum/ewm/service/request/model/Participation.java
@@ -0,0 +1,38 @@
+package ru.practicum.ewm.service.request.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.ewm.service.event.model.Event;
+import ru.practicum.ewm.service.user.model.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "participation_requests")
+public class Participation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "participation_id", nullable = false)
+    private long id;
+
+    @Column(name = "created", nullable = false)
+    private LocalDateTime created;
+
+    @JoinColumn(name = "event_id", referencedColumnName = "event_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Event event;
+
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Column(name = "status", nullable = false)
+    private String status;
+}
