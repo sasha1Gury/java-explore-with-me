c5ae4d5ff2083b73c40d0e433db78486acfb7d5b
sasha1Gury <sashagury456123@mail.ru>
2024-06-13 10:12:31 +0300

    feat: add methods CategoryAdminController

--- ewm-service/src/main/java/ru/practicum/ewm/service/category/CategoryPublicController.java
+++ ewm-service/src/main/java/ru/practicum/ewm/service/category/CategoryPublicController.java
@@ -1,4 +1,37 @@
 package ru.practicum.ewm.service.category;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.ewm.service.category.dto.CategoryDto;
+import ru.practicum.ewm.service.category.service.CategoryService;
+
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/categories")
+@Slf4j
+@RequiredArgsConstructor
+@Validated
 public class CategoryPublicController {
+    private final CategoryService categoryService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<CategoryDto> getAllCategories(@PositiveOrZero @RequestParam(defaultValue = "0") int from,
+                                              @Positive @RequestParam(defaultValue = "10") int size) {
+        log.info("Public get all categories: from = {}, size = {}", from, size);
+        return categoryService.getAllCategories(from, size);
+    }
+
+    @GetMapping("/{categoryId}")
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryDto getCategoryById(@Positive @PathVariable long categoryId) {
+        log.info("Public get category: categoryId = {}", categoryId);
+        return categoryService.getCategoryById(categoryId);
+    }
 }
