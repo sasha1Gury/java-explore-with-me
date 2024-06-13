dc25908412776025282271dec0b4ebdfd936ac94
sasha1Gury <sashagury456123@mail.ru>
2024-06-13 10:12:20 +0300

    feat: add methods CategoryAdminController

--- ewm-service/src/main/java/ru/practicum/ewm/service/category/CategoryAdminController.java
+++ ewm-service/src/main/java/ru/practicum/ewm/service/category/CategoryAdminController.java
@@ -1,4 +1,47 @@
 package ru.practicum.ewm.service.category;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.ewm.service.category.dto.CategoryDto;
+import ru.practicum.ewm.service.category.service.CategoryService;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+
+@RestController
+@RequestMapping(path = "/admin/categories")
+@Slf4j
+@RequiredArgsConstructor
+@Validated
 public class CategoryAdminController {
+    private final CategoryService categoryService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CategoryDto create(@Valid @RequestBody CategoryDto categoryDto) {
+
+        log.info("Admin create category: {}", categoryDto);
+        CategoryDto result = categoryService.create(categoryDto);
+        log.info("Admin create category result: {}", result);
+        return result;
+    }
+
+    @PatchMapping("/{categoryId}")
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryDto update(@Valid @RequestBody CategoryDto categoryDto,
+                              @Positive @PathVariable long categoryId) {
+
+        log.info("Admin update category: {}, categoryId = {}", categoryDto, categoryId);
+        return categoryService.update(categoryDto, categoryId);
+    }
+
+    @DeleteMapping("/{categoryId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@Positive @PathVariable long categoryId) {
+        log.info("Admin delete category: categoryId = {}", categoryId);
+        categoryService.delete(categoryId);
+    }
 }
