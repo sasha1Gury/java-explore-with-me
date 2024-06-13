be9edd57d2a96b6998d8bb9f1ddcfcf517e0ae29
sasha1Gury <sashagury456123@mail.ru>
2024-06-13 16:57:08 +0300

    feat: add CompilationDto

--- ewm-service/src/main/java/ru/practicum/ewm/service/compilation/dto/CompilationDto.java
+++ ewm-service/src/main/java/ru/practicum/ewm/service/compilation/dto/CompilationDto.java
@@ -1,4 +1,29 @@
 package ru.practicum.ewm.service.compilation.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.ewm.service.event.dto.EventShortDto;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CompilationDto {
+    @PositiveOrZero
+    private long id;
+
+    private List<EventShortDto> events;
+
+    @NotNull
+    private Boolean pinned;
+
+    @NotEmpty
+    private String title;
 }
