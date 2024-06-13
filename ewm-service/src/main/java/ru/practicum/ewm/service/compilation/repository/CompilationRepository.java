260974a94976b9597df43f63bed6ba6304a0fb48
sasha1Gury <sashagury456123@mail.ru>
2024-06-13 16:53:36 +0300

    feat: add methods in CompilationPublicController

--- ewm-service/src/main/java/ru/practicum/ewm/service/compilation/repository/CompilationRepository.java
+++ ewm-service/src/main/java/ru/practicum/ewm/service/compilation/repository/CompilationRepository.java
@@ -1,7 +1,11 @@
 package ru.practicum.ewm.service.compilation.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.ewm.service.compilation.model.Compilation;
 
-public interface CompilationRepository extends JpaRepository<Long, Compilation> {
+import java.util.List;
+
+public interface CompilationRepository extends JpaRepository<Compilation, Long> {
+    List<Compilation> findByPinned(Boolean pinned, Pageable page);
 }
