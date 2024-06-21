31da82f3ac5a5cca0a68d59128bd723fa24ca58f
sasha1Gury <sashagury456123@mail.ru>
2024-06-22 01:21:12 +0300

    fix: fix error handler

--- stat-service/stat-server/src/main/java/ru/practicum/stat/server/error_handle/ErrorHandler.java
+++ stat-service/stat-server/src/main/java/ru/practicum/stat/server/error_handle/ErrorHandler.java
@@ -2,6 +2,7 @@ package ru.practicum.stat.server.error_handle;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,7 +17,8 @@ public class ErrorHandler {
     public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT);
 
-    @ExceptionHandler({ValidationException.class})
+    @ExceptionHandler({ValidationException.class,
+            MissingServletRequestParameterException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidationException(final RuntimeException e) {
         log.info(e.getMessage());
