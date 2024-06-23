704e8686b80bdc6dfc13fa9080113f6b323a2c4f
sasha1Gury <sashagury456123@mail.ru>
2024-06-23 21:29:44 +0300

    feat: style

--- ewm-service/src/main/java/ru/practicum/ewm/service/user/service/UserService.java
+++ ewm-service/src/main/java/ru/practicum/ewm/service/user/service/UserService.java
@@ -7,12 +7,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import ru.practicum.ewm.service.exceptions.UserEmailNotUniqueException;
+import ru.practicum.ewm.service.exceptions.UserNotFoundException;
 import ru.practicum.ewm.service.user.dto.UserDto;
 import ru.practicum.ewm.service.user.model.User;
 import ru.practicum.ewm.service.user.repository.UserRepository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -47,12 +47,8 @@ public class UserService {
     }
 
     public User getUserById(long userId) {
-        Optional<User> optionalUser = userRepository.findById(userId);
-        if (optionalUser.isEmpty()) {
-            throw new RuntimeException("Пользователь " + userId + " не найден");
-        } else {
-            return optionalUser.get();
-        }
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("Пользователь " + userId + " не наден"));
     }
 
     public void delete(Long userId) {
