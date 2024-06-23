bf7841801e409fd06dc45acef12d3e15e052f526
sasha1Gury <sashagury456123@mail.ru>
2024-06-23 21:30:32 +0300

    feat: add Comment

---
+++ ewm-service/src/main/java/ru/practicum/ewm/service/comments/model/Comment.java
@@ -0,0 +1,38 @@
+package ru.practicum.ewm.service.comments.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.ewm.service.event.model.Event;
+import ru.practicum.ewm.service.user.model.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id", nullable = false)
+    private long id;
+
+    @JoinColumn(name = "event_id", referencedColumnName = "event_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Event event;
+
+    @JoinColumn(name = "author_id", referencedColumnName = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User author;
+
+    @Column(name = "text", nullable = false)
+    private String text;
+
+    @Column(name = "created", nullable = false)
+    private LocalDateTime created;
+}
